f82e6308038709cdade1df50c4d676617ae52185
Stephanie Penaloza <s.penaloza@a1qa.com>
2025-12-14 20:35:31 -0600

    add click()

--- framework/elements/BaseElement.js
+++ framework/elements/BaseElement.js
@@ -1,9 +1,18 @@
 import ElementType from '../constants/ElementType.js';
 
 export default class BaseElement {
-  constructor(locator, name) {
-    this.locator = locator;
+  constructor(page, selector, name) {
+    this.page = page;
+    this.locator = page.locator(selector);
     this.name = name;
     this.type = ElementType.ELEMENT;
+  }
+
+  /**
+   * Click on element
+   * @returns {Promise<void>}
+   */
+  async click() {
+    await this.locator.click();
   }
 }
