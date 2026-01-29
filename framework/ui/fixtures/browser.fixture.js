28fa715368d65f76fd7c2744bc53a7e29f5a528d
Stephanie Penaloza <s.penaloza@a1qa.com>
2026-01-29 16:49:41 -0600

    add file download functionality

--- framework/ui/fixtures/browser.fixture.js
+++ framework/ui/fixtures/browser.fixture.js
@@ -1,22 +1,32 @@
 import { test as base, expect } from '@playwright/test';
 import Browser from '../browser/Browser.js';
 import { settings } from '../../utils/ConfigReader.js';
+import path from 'path';
+import fs from 'fs';
+
+const DOWNLOAD_DIR = path.resolve(settings.downloadDir);
 
 /**
  * @type {import('@playwright/test').TestType<{ customBrowser: Browser }>} 
  */
 export const test = base.extend({
   customBrowser: async ({ browser }, use) => {
-    
-    const context = await browser.newContext();
+    fs.mkdirSync(DOWNLOAD_DIR, { recursive: true });
+
+    const context = await browser.newContext({
+      acceptDownloads: true,
+      downloadsPath: DOWNLOAD_DIR,
+    });
     const page = await context.newPage();
     const myBrowser = new Browser(page);
 
     if (settings.baseUrl) {
-        await myBrowser.openUrl(settings.baseUrl);
+      await myBrowser.openUrl(settings.baseUrl);
     }
     await use(myBrowser);
     await context.close();
+
+    fs.rmSync(DOWNLOAD_DIR, { recursive: true, force: true });
   },
 });
 
