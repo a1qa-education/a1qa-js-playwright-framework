c6ea067a3a3e550aa59d1434362b242167184a7d
Denis Drozdov <ddrozdov@ethosrisk.com>
2026-07-20 11:38:49 +0300

    update file_upload

--- tests/file_upload.spec.js
+++ tests/file_upload.spec.js
@@ -8,15 +8,3 @@ import UploadedFilesPage from './pages/FileUploaderPages/UploadedFilesPage.js';
 const fileName = 'test.txt';
 
 // Implement test block
-test('Check that file is uploaded', async ({ customBrowser: browser }) => {
-  const testData = ConfigReader.getTestData();
-  const mainPage = new MainPage(browser.page);
-  await mainPage.clickNavigationLink('File Upload');
-
-  const fileUploaderPage = new FileUploaderPage(browser.page);
-  await fileUploaderPage.uploadFile(testData.fileForUpload);
-
-  const uploadedFilesPage = new UploadedFilesPage(browser.page);
-  const uploadedFileName = await uploadedFilesPage.getUploadedFileName();
-  expect(uploadedFileName).toEqual(fileName);
-});
