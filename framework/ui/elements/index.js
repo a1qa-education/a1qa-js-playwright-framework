7d3d07da18b7b75b54cbd1d8e9de324678802bc3
Denis Drozdov <d.drozdov@a1qa.com>
2026-09-07 08:45:01 +0300

    remove framework

--- framework/ui/elements/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Label.js';
-export * from './Button.js';
-export * from './TextBox.js';
-export * from './FileInput.js';
-export * from './Dropdown.js';
-export * from './Checkbox.js';
-export * from './helpers/ElementsList.js';
